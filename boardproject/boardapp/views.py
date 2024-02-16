179ac22ced8dbbd7cc456e135cc38e8ef3a040ce
zhongshanzuoxin <zhongshanzuoxin@gmail.com>
2024-02-16 21:13:35 +0900

    練習

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def signupfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'signup.html', {})
+        except IntegrityError:
+            return render(request, 'signup.html', {'error':'このユーザーはすでに登録されています'})
     return render(request, 'signup.html', {})
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'login.html', {'context': 'ログインしました'})
+        else:
+            return render(request, 'login.html', {'context': 'ログインに失敗しました'})
+    return render(request, 'login.html', {})
+
+def listfunc(request):
+    return render(request, 'list.html', {})
