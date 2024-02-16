9e76e5cf0caf78a644674f92451abd6c3ce2fe1e
zhongshanzuoxin <zhongshanzuoxin@gmail.com>
2024-02-16 23:52:08 +0900

    練習

--- boardproject/boardapp/urls.py
+++ boardproject/boardapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import signupfunc, loginfunc, listfunc, logoutfunc, detailfunc
+from .views import signupfunc, loginfunc, listfunc, logoutfunc, detailfunc, goodfunc, readfunc
 
 urlpatterns = [
     path('signup/', signupfunc, name='signup'),
     path('login/', loginfunc, name='login'),
     path('logout/', logoutfunc, name='logout'),
     path('list/', listfunc, name='list'),
-    path('detail/<int:pk>', detailfunc, name='detail')
+    path('detail/<int:pk>', detailfunc, name='detail'),
+    path('good/<int:pk>', goodfunc, name='good'),
+    path('read/<int:pk>', readfunc, name='read'),
 ]
--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -43,3 +43,20 @@ def listfunc(request):
 def detailfunc(request, pk):
     object = get_object_or_404(BoardModel, pk=pk)
     return render(request, 'detail.html', {'object': object})
+
+def goodfunc(request, pk):
+    object = BoardModel.objects.get(pk=pk)
+    object.good = object.good + 1
+    object.save()
+    return redirect('list')
+
+def readfunc(request, pk):
+    object = get_object_or_404(BoardModel, pk=pk)
+    username = request.user.get_username()
+    if username in object.readtext:
+        return redirect('list')
+    else:
+        object.read = object.read + 1
+        object.readtext = object.readtext + ' ' + username
+        object.save()
+        return redirect('list')
